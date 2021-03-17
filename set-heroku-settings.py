e104d6eb085624a3406c0b83bfadf4f12570d43d
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-03-17 09:45:34 +0100

    remove setting AWS, psiturk keys as this is not needed on heroku + better to minimize potential to leak

--- set-heroku-settings.py
+++ set-heroku-settings.py
@@ -1,14 +1,14 @@
-from psiturk.psiturk_config import PsiturkConfig
+#from psiturk.psiturk_config import PsiturkConfig
 import subprocess
 
-CONFIG = PsiturkConfig()
-CONFIG.load_config()
+#CONFIG = PsiturkConfig()
+#CONFIG.load_config()
 
-sections = ['psiTurk Access','AWS Access']
-for section in sections:
-    for item in CONFIG.items(section):
-        #print 'heroku config:set ' + '='.join(item)
-        subprocess.call('heroku config:set ' + '='.join(item), shell=True)
+#sections = ['psiTurk Access','AWS Access']
+#for section in sections:
+#    for item in CONFIG.items(section):
+#        #print 'heroku config:set ' + '='.join(item)
+#        subprocess.call('heroku config:set ' + '='.join(item), shell=True)
         
 
 subprocess.call('heroku config:set ON_HEROKU=true', shell=True)
