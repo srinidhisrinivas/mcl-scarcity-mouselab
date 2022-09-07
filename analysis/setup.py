e26f391cbbf9d5fe671a6d31547e86d456464114
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2022-09-07 14:31:10 +0200

    analysis folder

---
+++ analysis/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="mouselab",
+    version="0.0.1",
+    packages=["mouselab", "mouselab.envs"],
+    url="",
+    license="",
+    author="Fred Callaway, Sayan Gul, Yash Raj Jain",
+    author_email="",
+    description="",
+    setup_requires=["wheel"],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "contexttimer",
+        "dill",
+        "gym",
+        "IPython",
+        "joblib",
+        "keras",
+        "matplotlib",
+        "more_itertools",
+        "networkx",
+        "pandas",
+        "pydantic",
+        "seaborn",
+        "toolz",
+        "tqdm",
+        "parameterized",
+    ],
+)
